07188e575362578628799831bc0fd18661b3b6c9
Lucky Jones Uayan <luckyjonesbuayan@gmail.com>
2022-11-24 14:06:19 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+// // HIDING NAVS
+// const navbar = document.getElementById("navbar");
+// let lastScrollY = window.scrollY;
+//
+// window.addEventListener("scroll", () => {
+//   hideNavOnScroll();
+//   showNavOnLastScrollPos()
+// })
+//
+//
+// const showNavOnLastScrollPos = () => {
+//   let scrollMaxY = window.scrollMaxY || (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+//   if (lastScrollY >= scrollMaxY) {
+//     navbar.classList.remove("hidden");
+//   }
+// }
+
+
+
+const floatRightElements = document.querySelectorAll(".float-right");
+const fadedElements = document.querySelectorAll(".fade");
+const progbar = document.querySelectorAll(".progbar");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  })
+})
+const progGrowObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("prog-grow");
+    } else {
+      entry.target.classList.remove("prog-grow");
+    }
+  })
+})
+
+floatRightElements.forEach((el) => observer.observe(el));
+fadedElements.forEach((el) => observer.observe(el));
+progbar.forEach((el) => progGrowObserver.observe(el));
+
+
+const hideNavOnScroll = () => {
+  if (lastScrollY < window.scrollY) {
+    navbar.classList.add("hidden");
+  } else {
+    navbar.classList.remove("hidden");
+  }
+  lastScrollY = window.scrollY;
+}
+
+
+
+// CURSOR EVENTS
+// let cursor = document.getElementById("cursor");
+//
+// document.addEventListener("mousemove", (e) => {
+//   followMouse(e)
+// })
+//
+//
+// const followMouse = (e) => {
+//   let x = e.clientX;
+//   let y = e.clientY;
+//
+//   cursor.style.left = x + "px";
+//   cursor.style.top = y + "px";
+// }
